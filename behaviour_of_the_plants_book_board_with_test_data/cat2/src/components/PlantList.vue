dcce68973247ebbff97be9c5c7880d5e28a68c2d
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-11-13 13:52:05 +0100

    finished PlantList component

--- behaviour_of_the_plants_book_board_with_test_data/cat2/src/components/PlantList.vue
+++ behaviour_of_the_plants_book_board_with_test_data/cat2/src/components/PlantList.vue
@@ -1,11 +1,22 @@
 <script>
+import PlantItem from "@/components/PlantItem.vue";
+
 export default {
-  name: "PlantList"
+  name: "PlantList",
+  components: {PlantItem},
+  props: {
+    plantList: {
+      type: Array,
+      required: true
+    }
+  }
 }
 </script>
 
 <template>
-
+  <div class="plant-list">
+    <plant-item v-for="(plant, index) in plantList" :plant="plant" :key="plant.id"></plant-item>
+  </div>
 </template>
 
 <style scoped>
