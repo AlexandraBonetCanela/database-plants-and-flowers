d5eb3c62215893c8dc1eb2d6281e5b8b822d5693
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-11-13 13:18:54 +0100

    finished PlantItem

--- behaviour_of_the_plants_book_board_with_test_data/cat2/src/components/PlantItem.vue
+++ behaviour_of_the_plants_book_board_with_test_data/cat2/src/components/PlantItem.vue
@@ -1,11 +1,80 @@
 <script>
+/**
+ * @typedef {Object} PlantInfo
+ * @property {number} id - Plant ID
+ * @property {string} name - Plant Name
+ * @property {string} description - Plant Description
+ * @property {string} image - Plant Image URL
+ * @property {string} date - Plant Date (YYYY-MM-DD)
+ * @property {string} family - Plant Family
+ * @property {string} genus - Plant Genus
+ * @property {string} species - Plant Species
+ * @property {string[]} labels - Plant Labels
+ * @property {boolean} favorite - Plant Favorite
+ * @property {number} rating - Plant Rating (0-5)
+ * @property {string} personalNote - Plant Personal Note
+ */
 export default {
-  name: "PlantItem"
+  name: "PlantItem",
+  props: {
+    plant: {
+      type: Object,
+      required: true,
+      validator(value) {
+        return (
+            typeof value.id === 'number' &&
+            typeof value.name === 'string' &&
+            typeof value.description === 'string' &&
+            typeof value.image === 'string' &&
+            typeof value.date === 'string' &&
+            typeof value.family === 'string' &&
+            typeof value.genus === 'string' &&
+            typeof value.species === 'string' &&
+            Array.isArray(value.labels) && value.labels.every(label => typeof label === 'string') &&
+            typeof value.favorite === 'boolean' &&
+            typeof value.rating === 'number' && value.rating >= 0 && value.rating <= 5 &&
+            typeof value.personalNote === 'string'
+        );
+      }
+    }
+  }
 }
 </script>
 
 <template>
+  <div :class="['plant-item', {'plant-item--favourite': plant.favorite}]">
 
+    <button class="plant-item__delete" datasrc="">
+      <img src="@/assets/delete-button.svg" alt="Delete plant"/>
+    </button>
+
+    <div class="plant-item__image">
+      <img :src="plant.image" alt="Plant image"/>
+    </div>
+
+    <div class="plant-item__info">
+      <h2 class="plant-item__title">{{plant.name}}</h2>
+
+      <div class="plant-item__data">
+        <p class="plant-item__date">Date: {{plant.date}}</p>
+        <p class="plant-item__family">Family: {{plant.family}}</p>
+        <p class="plant-item__genus">Genus: {{plant.genus}}</p>
+        <p class="plant-item__species">Species: {{plant.species}}</p>
+      </div>
+
+      <p class="plant-item__description">{{plant.description}}</p>
+
+      <p class="plant-item__personal-note">{{plant.personalNote}}</p>
+
+      <div class="plant-item__extra">
+        <p class="plant-item__rating">{{plant.rating}}/5</p>
+        <p class="plant-item__labels">
+          <span v-for="(label, index) in plant.labels" :key="index">{{label}}</span>
+        </p>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <style scoped>
